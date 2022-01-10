0e9bd2702a3c24a17a4e4278adce91e24cd24d1a
TomLeCollegue <tomkubasik.pro@gmail.com>
2022-01-10 23:16:40 +0100

    add explanation

--- src/exercice2/MainExercice2.java
+++ src/exercice2/MainExercice2.java
@@ -16,26 +16,40 @@ public class MainExercice2 implements Exo2 {
 
     // EXO 2
     /*
-     * Recupérez depuis le serveur la liste des gens qui sont nés à Chambéry,
+     * Recupérez depuis le serveur la liste des gens qui sont nés à Chambéry.
      * La récupération de Data se fait dans l'ordre :
      *  - Récupération de data sur le server grace à TxtHelper.getDataFromTxt("server.txt");
-     *  - Vider la base de données locale 
+     *  - Vider la base de données locale grace a fonction TxtHelper.clearDataLocal();
+     *  - Insérer les données dans la database locale
+     *  - Afficher les données locale grace à TxtHelper.getDataFromTxt(data, "local.txt")
      */
     @Override
     public void displayPersonFromChambery() {
         System.out.println(business.getPersonsFromCity(Constants.CHAMBERY));
     }
 
+    /*
+     * Recupérez depuis le serveur la liste des gens qui ont plus de 25 ans.
+     * La récupération de Data se fait comme dans la question précedente
+     */
     @Override
     public void displayBoomers() {
         System.out.print(business.getBoomers());
     }
 
+    /*
+     * Recupérez depuis le serveur la liste des gens de sexe féminin.
+     * La récupération de Data se fait comme dans la question précedente
+     */
     @Override
     public void displayFemales() {
         System.out.println(business.getFemales());
     }
 
+    /*
+     * Recupérez depuis le serveur la liste des femmes de plus de 25 ans.
+     * La récupération de Data se fait comme dans la question précedente
+     */
     @Override
     public void displayFemaleBoomers() {
         System.out.println(business.getFemaleBoomers());
