cda5d71a17983984aa60de2d9a2e6fe46790f7c3
tristan <tristan.porteries@gmail.com>
2022-01-11 08:36:44 +0100

    feat: exercice 1

--- src/exercice1/MainExercice1.java
+++ src/exercice1/MainExercice1.java
@@ -1,17 +1,37 @@
 package exercice1;
 
 import datamocklib.Person;
+import datamocklib.TxtHelper;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class MainExercice1 {
 
+    public static Person parse(String line) {
+        String[] tokens = line.split(",");
+        return new Person(tokens[0], tokens[1], tokens[2], tokens[3], tokens[4], tokens[5]);
+    }
+
     // Format du fichier : firstName,lastName,birthDate,gender,cityOfResidence,cityOfBirth
     // Pas de header
-    public List<Person> parse(List<String> data) {
+    public static List<Person> parse(List<String> data) {
         // A COMPLETER
-        return new ArrayList<Person>();
+        return data.stream().map(line -> parse(line)).collect(Collectors.toList());
+    }
+
+    public static Map<String, Integer> countPersonPerCities(List<Person> persons) {
+        HashMap<String, Integer> countPerCities = new HashMap<>();
+        for (Person person : persons) {
+            String cityOfBirth = person.getCityOfBirth();
+            int count = countPerCities.getOrDefault(cityOfBirth, 0);
+            countPerCities.put(cityOfBirth, count + 1);
+        }
+
+        return countPerCities;
     }
 
     public static void main(String[] args) {
@@ -24,6 +44,10 @@ public class MainExercice1 {
          * Les données contiennent une trentaine de villes, Bonne chance !
         */
 
-
+        String fileName = args[0];
+        List<String> fileData = TxtHelper.getDataFromTxt(fileName);
+        List<Person> persons = parse(fileData);
+        Map<String, Integer> countPerCities = countPersonPerCities(persons);
+        countPerCities.forEach((city, count) -> System.out.println(city + ":" + count));
     }
 }
